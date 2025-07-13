90e73f1067e529edd2a3fe445b61748cb5560990
br7forlife <diazpachecobrayan@gmail.com>
2025-07-12 22:23:41 -0500

    started project dashboard

---
+++ dashboardWeather/tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ["./src/**/*.{html,js,astro}"],
+  theme: {
+    extend: {
+      colors: {
+        "grad-1": "#0b1c36",
+        "grad-2": "#274665",
+        "grad-3": "#457697",
+        "grad-4": "#65a9c8",
+        "grad-5": "#8ae0f9",
+      },
+    },
+  },
+  plugins: [],
+};
